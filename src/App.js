9f9cbe593cabb680617b466c24690bb16bcf5f18
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-09-20 13:50:12 +0000

    style

--- src/App.js
+++ src/App.js
@@ -14,14 +14,12 @@ function App() {
           title="Product"
                 />
       <CardContent>
-        <div className={classes.cardContent}>
-          <Typography variant='h5' gutterBottom>
-            Product
-          </Typography>
-          <Typography variant='h5'>
-            28,99 zł
-          </Typography>
-        </div>
+        <Typography variant='h5' gutterBottom>
+          Product
+        </Typography>
+        <Typography variant='h4' gutterBottom>
+          28,99 zł
+        </Typography>
         <Typography variant="body2" color="textSecondary">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum    has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to       make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining              essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more      recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </Typography>
       </CardContent>
