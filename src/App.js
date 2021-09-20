82f006e50f5cca1917e045fb8b5b142f2e9f1090
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-09-20 11:01:39 +0000

    delete "api" variables

--- src/App.js
+++ src/App.js
@@ -16,16 +16,16 @@ function App() {
       <CardContent>
         <div className={classes.cardContent}>
           <Typography variant='h5' gutterBottom>
-                      {product.name}
+            Product
           </Typography>
           <Typography variant='h5'>
-                      {product.price.formatted_with_symbol}
+            28,99 zł
           </Typography>
         </div>
           <Typography variant="body2" color="textSecondary">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Typography>
       </CardContent>
         <CardActions disableSpacing className={classes.cardActions}>
-          <IconButton aria-label="Dodaj do koszyka" onClick={handleAddToCart}>
+          <IconButton aria-label="Dodaj do koszyka">
             <AddShoppingCart />
           </IconButton>
         </CardActions>
