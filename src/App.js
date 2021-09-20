b8764fd4770c0354501c64e4211922c1378bfcd6
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-09-20 11:10:04 +0000

    styling img page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     <Card className={classes.root}>
       <CardMedia
           className={classes.media}
+          component={img}
           image="https://cdn.pixabay.com/photo/2017/07/28/16/30/bee-pollen-2549125_960_720.jpg"
           title="Product"
                 />
